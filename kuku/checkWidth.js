e6ec061f7260544f86634863cad26848efca4214
fumiyasu-aizu <114791722+fumiyasu-aizu@users.noreply.github.com>
2023-05-23 17:56:43 +0900

    Add files via upload
    追加と修正

---
+++ kuku/checkWidth.js
@@ -0,0 +1,18 @@
+﻿var typeWidth = 340;
+
+if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPod') > 0 ||    navigator.userAgent.indexOf('Android') > 0 && userAgent.indexOf('Mobile') > 0) {
+  typeWidth = 320;
+}
+/*
+if (screen.availWidth >= 1200) {
+  typeWidth = 340;
+}
+*/
+function startKuku() {
+  if (typeWidth == 320) {
+     location.href = './xxKukuW320.html';
+  } else {
+     location.href = './xxKukuW340.html';
+  }
+}
+
